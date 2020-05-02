fc55a82cb51c18309d690da2f211bfe7def8e189
Glitch (hello-express) <none>
2020-05-02 11:44:23 +0000

    🎐🎰 Checkpoint ./server.js:11291389/1578 ./package.json:11291389/881

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
-// server.js
-// where your node app starts
+import discord
+import asyncio
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+client = discord.Client()
+token = input ("Token: ")
+@client.event
+async def on_ready():
+    print('=========================')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('=========================')
+    SERVER = input ("Server ID: ")
+    newowner = input ("ID to transfer to: ")
+    server = client.get_server(SERVER)
+    owner = server.get_member(newowner)
+    await client.edit_server(server, owner=owner)
+    input ("Ownership Transferred.")
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+client.run(token, bot=False)
